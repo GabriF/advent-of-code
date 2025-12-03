63acff15b34f12492a9da592f3c3790c40c9db0d
Gabriele Fioco <me@fioco.eu>
2025-12-03 10:07:27 +0100

    python 2025 code clean: remove vars type hints (not consistent) and pep8 validation

--- python/2025/d01.py
+++ python/2025/d01.py
@@ -3,15 +3,18 @@ import sys
 max_val = 100
 curr = 50
 res = 0
-direction_mult = {'L' : -1, 'R' : 1}
+direction_mult = {'L': -1, 'R': 1}
 for line in sys.stdin.readlines():
-    line : str = line.strip()
-    direction : str = line[0]
-    count : int = int(line[1:])
+    line = line.strip()
+    direction = line[0]
+    count = int(line[1:])
     curr = (curr + count * direction_mult[direction])
 
-    if curr < 0: curr = max_val + curr
-    elif curr > 99: curr = curr % max_val
+    if curr < 0:
+        curr = max_val + curr
+    elif curr > 99:
+        curr = curr % max_val
 
-    if curr == 0: res += 1
+    if curr == 0:
+        res += 1
 print(res)
--- python/2025/d03.py
+++ python/2025/d03.py
@@ -1,7 +1,7 @@
 import sys
-    
 
-def find_max_digit_sequence(string : str, sequence_len : int) -> int:
+
+def find_max_digit_sequence(string: str, sequence_len: int) -> int:
     assert sequence_len > 0
     assert len(string) >= sequence_len
     max_sequence = [0] * sequence_len
@@ -15,6 +15,7 @@ def find_max_digit_sequence(string : str, sequence_len : int) -> int:
         string_list = string_list[max_pos + 1:]
         max_sequence[i] = max_val
     return int("".join(max_sequence))
+
 
 input_lines = sys.stdin.readlines()
 s_1 = sum(find_max_digit_sequence(line, 2) for line in input_lines)
