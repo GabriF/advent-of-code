47f31e9e1c7120e9a1c66d65da408d984b99b4bb
Gabriele Fioco <me@fioco.eu>
2025-12-03 10:03:35 +0100

    python 2025 new day 03.2

--- python/2025/d03.py
+++ python/2025/d03.py
@@ -1,13 +1,22 @@
 import sys
+    
 
+def find_max_digit_sequence(string : str, sequence_len : int) -> int:
+    assert sequence_len > 0
+    assert len(string) >= sequence_len
+    max_sequence = [0] * sequence_len
+    string_list = [c for c in string.strip()]
+    for i in range(sequence_len):
+        string_list_len = len(string_list)
+        max_pos, max_val = max(
+            enumerate(string_list[:string_list_len - sequence_len + i + 1]),
+            key=lambda x: x[1]
+        )
+        string_list = string_list[max_pos + 1:]
+        max_sequence[i] = max_val
+    return int("".join(max_sequence))
 
-def find_max_digit_pair(string : str, ) -> int:
-    max = 0
-    for position_1, digit_1 in enumerate(string):
-        for digit_2 in string[position_1 + 1:]:
-            curr_num = int(digit_1 + digit_2)
-            if max < curr_num: max = curr_num
-    return max
-
-res = sum(find_max_digit_pair(line) for line in sys.stdin.readlines())
-print(res)
+input_lines = sys.stdin.readlines()
+s_1 = sum(find_max_digit_sequence(line, 2) for line in input_lines)
+s_2 = sum(find_max_digit_sequence(line, 12) for line in input_lines)
+print((s_1, s_2))
