85f9f8c2010cba889ec785a4b2445058bca0f880
Gabriele Fioco <me@fioco.eu>
2025-12-05 06:42:49 +0100

    python 2025 new day 05.2

--- python/2025/d05.py
+++ python/2025/d05.py
@@ -1,24 +1,64 @@
 import sys
 
-lines = sys.stdin.read().strip().split("\n")
-ranges = list()
-id_start = 0
-for line in lines:
-    id_start += 1
-    line = line.strip()
-    if line == "": break
-    tokens = line.split("-")
-    start = int(tokens[0])
-    end = int(tokens[1])
-    ranges.append(range(start, end + 1))
 
-s = 0
-for line in lines[id_start:]:
-    line = line.strip()
-    val = int(line)
-    for r in ranges:
-        if val in r:
-            s += 1
+def is_within(x: int, r: tuple[int, int]) -> bool:
+    return r[0] <= x <= r[1]
+
+
+def _range_overlap_helper(r_1: tuple[int, int], r_2: tuple[int, int]):
+    return is_within(r_1[0], r_2) or is_within(r_1[1], r_2)
+
+
+def range_overlap(r_1: tuple[int, int], r_2: tuple[int, int]) -> bool:
+    return _range_overlap_helper(r_1, r_2) or _range_overlap_helper(r_2, r_1)
+
+
+def build_ranges(l: list[str]) -> list[tuple[int, int]]:
+    ranges_list = []
+    for record in l:
+        tokens = record.split("-")
+        v_1 = int(tokens[0])
+        v_2 = int(tokens[1])
+        add_range(ranges_list, (v_1, v_2))
+    return ranges_list
+
+
+def add_range(ranges_list: list[tuple[int, int]], new_range: tuple[int, int]):
+    for i, curr_range in enumerate(ranges_list):
+        if range_overlap(new_range, curr_range):
+            new_range = (min(new_range[0], curr_range[0]), max(
+                new_range[1], curr_range[1]))
+            ranges_list.pop(i)
+            add_range(ranges_list, new_range)
+            return
+    ranges_list.append(new_range)
+
+
+def read_input(input_string: str) -> tuple[list[str], list[str]]:
+    lines = input_string.strip().split("\n")
+    ingredients_ranges = []
+    ingredients_list = []
+    for i, line in enumerate(lines):
+        if line == "":
+            ingredients_list = lines[i+1:]
+            break
+        ingredients_ranges.append(line)
+    return (ingredients_ranges, ingredients_list)
+
+
+ingredients_ranges, ingredients_list = read_input(sys.stdin.read())
+ranges_list = build_ranges(ingredients_ranges)
+
+s_1 = 0
+for val in ingredients_list:
+    val_int = int(val)
+    for r in ranges_list:
+        if is_within(val_int, r):
+            s_1 += 1
             break
 
-print(s)
+s_2 = 0
+for r in ranges_list:
+    s_2 += r[1] - r[0] + 1
+
+print((s_1, s_2))
