cb69fe25cbbac574592c488896cf20ac059105e8
Gabriele Fioco <me@fioco.eu>
2025-12-05 20:47:24 +0100

    python 2025 refactor day 03

--- python/2025/d03.py
+++ python/2025/d03.py
@@ -1,23 +1,29 @@
 import sys
 
 
-def find_max_digit_sequence(string: str, sequence_len: int) -> int:
-    assert sequence_len > 0
-    assert len(string) >= sequence_len
-    max_sequence = [0] * sequence_len
-    string_list = [c for c in string.strip()]
-    for i in range(sequence_len):
-        string_list_len = len(string_list)
+def find_max_digit_sequence(string: str, number_of_digits: int) -> int:
+    assert number_of_digits > 0
+    assert len(string) >= number_of_digits
+
+    max_sequence = []
+
+    for i in range(number_of_digits):
+        string_len = len(string)
+
+        sub_str = string[:string_len - number_of_digits + i + 1]
         max_pos, max_val = max(
-            enumerate(string_list[:string_list_len - sequence_len + i + 1]),
+            enumerate(sub_str),
             key=lambda x: x[1]
         )
-        string_list = string_list[max_pos + 1:]
-        max_sequence[i] = max_val
+
+        string = string[max_pos + 1:]
+        max_sequence.append(max_val)
     return int("".join(max_sequence))
 
 
 input_lines = sys.stdin.readlines()
+
 s_1 = sum(find_max_digit_sequence(line, 2) for line in input_lines)
 s_2 = sum(find_max_digit_sequence(line, 12) for line in input_lines)
+
 print((s_1, s_2))
