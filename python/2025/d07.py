8a80bad91f39c32b1925867e2ad55c1b811c75a3
Gabriele Fioco <me@fioco.eu>
2025-12-07 22:31:56 +0100

    python 2025 new day 07.2

--- python/2025/d07.py
+++ python/2025/d07.py
@@ -39,18 +39,51 @@ def split(beams: list[int], beam: int) -> bool:
     return True
 
 
-lines = sys.stdin.read().split("\n")
+def path_calculator(pos: int, lines: list[str]) -> int:
+    return 1 + _path_calculator_helper(pos, 0, lines, {})
 
-beams = []
+
+def _path_calculator_helper(pos: int, current_line: int, lines: list[str], mem: dict[tuple[str, str], int]) -> int:
+    if current_line == len(lines) - 1:
+        return 0
+
+    if (pos, current_line) in mem.keys():
+        return mem[(pos, current_line)]
+
+    line = lines[current_line]
+    val = line[pos]
+    
+    if val != "^":
+        return _path_calculator_helper(pos, current_line + 1, lines, mem)
+    
+    tot = 1
+    if pos > 0:
+        tot += _path_calculator_helper(pos - 1,
+                                        current_line + 1, lines, mem)
+    if pos < len(lines):
+        tot += _path_calculator_helper(pos + 1,
+                                        current_line + 1, lines, mem)
+    mem[((pos, current_line))] = tot
+    return tot
+    
+        
+
+lines = sys.stdin.read().strip().split("\n")
+
 for i, val in enumerate(lines[0]):
     if val == "S":
-        beams.append(i)
+        start = i
+        break
 
 s_1 = 0
+beams = [start]
 for line in lines[1:]:
     for i, val in enumerate(line):
         if val == "^":
             if split(beams, i):
                 s_1 += 1
 
-print(s_1)
+
+s_2 = path_calculator(start, lines)
+
+print((s_1, s_2))
